9c37bfda4630c5cb1f689394be18fbb392313f39
Cristhian Avila <cyavilap@hotmail.com>
2021-03-17 17:50:29 -0500

    First commit

---
+++ pageActions.js
@@ -0,0 +1,62 @@
+var selectedModule = "All";
+var filterMode = "Grid";
+const menuOptions = ["All", "Branding", "Web", "Photography", "App"];
+const imagesMenus = {
+  All: [
+    "ambitious-creative-co-rick-barrett-Hw7hK-kYA-Q-unsplash.jpg",
+    "daniela-izotenko-hOhlYhAiizc-unsplash.jpg",
+    "docusign-s5z5vEdbHmM-unsplash.jpg",
+    "emilio-garcia-lb9hi0NDjT0-unsplash.jpg",
+    "filippo-bortolotto-g8aGsBf7iu4-unsplash.jpg",
+    "javier-esteban-zFT7i1-pKwY-unsplash.jpg",
+    "lisha-riabinina-zS0P7o9bJm4-unsplash.jpg",
+    "remi-thorel-DgUD5P1a2Hc-unsplash.jpg",
+    "remi-thorel-UEoKkhdkp9I-unsplash.jpg",
+    "surface-QZzA_JgREC4-unsplash.jpg",
+  ],
+  Branding: [
+    "docusign-s5z5vEdbHmM-unsplash.jpg",
+    "emilio-garcia-lb9hi0NDjT0-unsplash.jpg",
+  ],
+  Web: [
+    "filippo-bortolotto-g8aGsBf7iu4-unsplash.jpg",
+    "javier-esteban-zFT7i1-pKwY-unsplash.jpg",
+  ],
+  Photography: [
+    "lisha-riabinina-zS0P7o9bJm4-unsplash.jpg",
+    "remi-thorel-DgUD5P1a2Hc-unsplash.jpg",
+  ],
+  App: [
+    "remi-thorel-UEoKkhdkp9I-unsplash.jpg",
+    "surface-QZzA_JgREC4-unsplash.jpg",
+  ],
+};
+
+function setSelectedModule(moduleName) {
+  selectedModule = moduleName;
+  paintMenuOptions();
+}
+
+function paintMenuOptions() {
+  let buttonsContainer = document.getElementsByClassName("buttons-container");
+  for (const buttonContainer of buttonsContainer) {
+    let menuOptionsHtml = "";
+    menuOptions.forEach((option) => {
+      menuOptionsHtml += `<div 
+        class="menu-button ${
+          option === selectedModule ? "menu-button-selected" : ""
+        }" onclick="setSelectedModule('${option}')"><span>${option}</span></div>`;
+    });
+    buttonContainer.innerHTML = menuOptionsHtml;
+  }
+  paintImages();
+}
+
+function paintImages() {
+  let scrollImage = document.getElementById("scroll-images");
+  let imagesHtml = "";
+  imagesMenus[selectedModule].forEach((url, index) => {
+    imagesHtml += `<li><img loading="lazy" alt="Image-${index}" src="./images/${url}"/></li>`;
+  });
+  scrollImage.innerHTML = imagesHtml;
+}
